9c95c392ec7b40634535fa6a2ab3e1d7c0259dba
Sơn Nguyễn <son@sonng.dev>
2023-01-07 18:37:08 +0000

    feat: Change default health message

--- src/index.ts
+++ src/index.ts
@@ -7,7 +7,7 @@ const app = express();
 const port = process.env.PORT;
 
 app.get('/', (req, res) => {
-  res.send('Express + TypeScript Server');
+  res.send('ChatGPT server is running OK');
 });
 
 app.listen(port, () => {
