fa2d825c26700e6cad8d5a28d05e60cdacac7a8e
Sơn Nguyễn <son@sonng.dev>
2023-01-09 20:32:35 +0000

    chore: Improve tsconfig to allow top-level await

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,17 @@
 import express from 'express';
 import dotenv from 'dotenv';
-import { getChatGPTAPI, ChatGPTAPIBrowser } from './chatgpt.js';
+import { getChatGPTAPI } from './chatgpt.js';
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
-let api: ChatGPTAPIBrowser | null = null;
+const api = await getChatGPTAPI();
 
 app.get('/', (req, res) => {
   res.send('ChatGPT server is running OK');
 });
 
-app.listen(port, async () => {
-  api = await getChatGPTAPI();
+app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
 });
