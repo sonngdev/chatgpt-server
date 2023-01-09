d4bc715cf7657ca1a7254c5f92e1563a7e65b7a3
Sơn Nguyễn <son@sonng.dev>
2023-01-09 21:39:44 +0000

    feat: Make request to ChatGPT API

--- src/index.ts
+++ src/index.ts
@@ -15,7 +15,7 @@ dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
-// const api = await getChatGPTAPI();
+const api = await getChatGPTAPI();
 
 app.use(cors());
 app.use(express.json());
@@ -26,7 +26,7 @@ app.get('/', (req, res) => {
 
 app.post(
   '/chatgpt/messages',
-  (
+  async (
     req: Request<
       {},
       CreateChatGPTMessageResponse,
@@ -34,8 +34,23 @@ app.post(
     >,
     res,
   ) => {
-    const { body } = req;
-    res.json({ answer: body.text });
+    const { text } = req.body;
+
+    try {
+      const answer = await api.sendMessage(text);
+      res.json({ answer: answer.response });
+    } catch (error: unknown) {
+      let errorMessage: string;
+      if (typeof error === 'string') {
+        errorMessage = error;
+      } else if (error instanceof Error) {
+        errorMessage = error.message;
+      } else {
+        errorMessage = 'Something went wrong';
+      }
+
+      res.status(500).json({ answer: errorMessage });
+    }
   },
 );
 
