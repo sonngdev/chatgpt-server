9e17d0c6848338c88271a9f9d422ae055c6eebfa
Sơn Nguyễn <son@sonng.dev>
2023-01-09 21:18:32 +0000

    feat: Prepare endpoint to create ChatGPT message

--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,43 @@
-import express from 'express';
+import express, { Request } from 'express';
 import dotenv from 'dotenv';
+import cors from 'cors';
 import { getChatGPTAPI } from './chatgpt.js';
+
+interface CreateChatGPTMessageRequestBody {
+  text: string;
+}
+
+interface CreateChatGPTMessageResponse {
+  answer: string;
+}
 
 dotenv.config();
 
 const app = express();
 const port = process.env.PORT || 8000;
-const api = await getChatGPTAPI();
+// const api = await getChatGPTAPI();
+
+app.use(cors());
+app.use(express.json());
 
 app.get('/', (req, res) => {
   res.send('ChatGPT server is running OK');
 });
+
+app.post(
+  '/chatgpt/messages',
+  (
+    req: Request<
+      {},
+      CreateChatGPTMessageResponse,
+      CreateChatGPTMessageRequestBody
+    >,
+    res,
+  ) => {
+    const { body } = req;
+    res.json({ answer: body.text });
+  },
+);
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
